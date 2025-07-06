9bd81dca3bf2b5b19f8e7c7f128d1c2299f44fca
Harshit Kumar Singh <163038346+HarshittSinghh@users.noreply.github.com>
2025-07-06 16:35:09 +0530

    Fifth Commit

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,21 +1,11 @@
-Output:
-#include <gtest/gtest.h>
-TEST(ArithmeticTests, Addition) {
-  EXPECT_EQ(add(3, 5), 8);
-}
-TEST(ArithmeticTests, Subtraction) {
-  EXPECT_EQ(subtract(6, 2), 4);
-}
-TEST(ArithmeticTests, Multiplication) {
-  EXPECT_EQ(multiply(3, 5), 15);
-}
-TEST(ArithmeticTests, Division) {
-  EXPECT_EQ(divide(18, 2), 9);
-}
-TEST(ArithmeticTests, Factorial) {
-  EXPECT_EQ(factorial(5), 120);
-}
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+The input code is a summary of the coverage data for a C++ project. It contains information about the files in the project, their functions, and the lines that have been executed during testing.
+Here's a breakdown of the input code:
+* `TN` stands for "Test Name" and indicates the name of the test case.
+* `SF` stands for "Source File" and indicates the source file containing the code to be tested.
+* `FN` stands for "Function Name" and indicates the function names in the source file that have been executed during testing.
+* `DA` stands for "Data Array" and indicates the lines of code that have been executed within a function. The first number is the line number, and the second number is the number of times the line was executed.
+* `FNDA` stands for "Function Data Array" and indicates the functions in the source file that have been executed during testing. The first number is the function name, and the second number is the number of times the function was called.
+* `LF` stands for "Line Feed" and indicates the total number of lines in the source files.
+* `LH` stands for "Line Hits" and indicates the total number of lines that have been executed during testing.
+* `end_of_record` is a marker at the end of each record.
+The input code provides detailed information about which functions and branches are missing test coverage, allowing developers to focus their efforts on improving test coverage for specific areas of the codebase.
